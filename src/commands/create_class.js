f3a47de4aea849c799f5130056069fe026e65cd9
Alvaro Videla <alvaro.videla@fauna.com>
2018-05-08 16:33:50 -0700

    refactors CreateClassCommand to use FaunaCommand

--- src/commands/create_class.js
+++ src/commands/create_class.js
@@ -1,34 +1,18 @@
-const {Command, flags} = require('@oclif/command')
-
+const {flags} = require('@oclif/command')
+const FaunaCommand = require('../lib/fauna_command.js')
 const faunadb = require('faunadb');
 const q = faunadb.query;
-const os = require('os');
-const path = require('path');
-const fs = require('fs')
 
-function getConfigFile() {
-	return path.join(os.homedir(), '.fauna-shell');
-}
-
-function getRootKey(fileName) {
-	return new Promise(function(resolve, reject){
-		fs.readFile(fileName, 'utf8', (err, data) => {
-			err ? reject(err) : resolve(data);
-		});
-	});
-}
-
-class CreateClassCommand extends Command {
+class CreateClassCommand extends FaunaCommand {
   async run() {
 	  const {flags} = this.parse(CreateClassCommand);
 	  const name = flags.name || 'default';
 		const database = flags.database || '';
 	  const log = this.log;
-	  
-	  getRootKey(getConfigFile())
-	  .then(function(rootKey) {
-		  log(`creating database ${name}`);
-	 	  var client = new faunadb.Client({ secret: "fnACuZ8SV1ACAul5EGuawxCgeVdOn8SdXYKiMPUZ" + ":" + database + ":admin" });
+		const dbScope = database;
+		const role = "admin";
+		
+		this.withClient(function(client) {
 		  client.query(q.CreateClass({ name: name }))
 		  .then(function(res) {
 			  log(res);
@@ -36,10 +20,7 @@ class CreateClassCommand extends Command {
 		  .catch(function(error) {
 			  log(error);
 		  });
-	  })
-	  .catch(function(error) {
-		  log(error);
-	  });
+		}, dbScope, role);
   }
 }
 
