53d88a4cf972413189a3b74313258695e5d10e46
Alvaro Videla <alvaro.videla@fauna.com>
2018-05-08 16:04:40 -0700

    refactors ListDatabasesCommand to use base FaunaCommand

--- src/commands/list_databases.js
+++ src/commands/list_databases.js
@@ -1,34 +1,21 @@
 const {Command, flags} = require('@oclif/command')
+const FaunaCommand = require('../lib/fauna_command.js')
 
+const {getRootKey, getConfigFile} = require('../lib/misc.js')
 const faunadb = require('faunadb');
 const q = faunadb.query;
-const os = require('os');
-const path = require('path');
-const fs = require('fs')
 
-function getRootKey() {
-	return "fnACuZ8SV1ACAul5EGuawxCgeVdOn8SdXYKiMPUZ";
-	var configFile = path.join(os.homedir(), '.fauna-shell');
-	fs.readFile(configFile, 'utf8', function (err, data) {
-	  if (err) {
-	    return console.log(err);
-	  }
-	  return data;
-	});
-}
-
-class ListDatabasesCommand extends Command {
+class ListDatabasesCommand extends FaunaCommand {
   async run() {
 	  const {flags} = this.parse(ListDatabasesCommand);
 	  const name = flags.name || 'default';
-	  const rootKey = getRootKey();
-	  this.log(rootKey);
-	  const log = this.log;
-	  var client = new faunadb.Client({ secret: rootKey });
-	  var helper = client.paginate(q.Databases(null));
-	  helper.each(function(page) {
-		  log(page);
-	  });
+		const log = this.log;
+		this.withClient(function (client) {
+		  var helper = client.paginate(q.Databases(null));
+		  helper.each(function(page) {
+			  log(page);
+		  });
+		})
   }
 }
 
