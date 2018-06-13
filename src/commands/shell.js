fe1a0f2ede8998bb79a30d31fe5dd1880dccf011
Alvaro Videla <alvaro.videla@fauna.com>
2018-06-13 19:04:37 +0200

    prevents starting a shell for a non existing database

--- src/commands/shell.js
+++ src/commands/shell.js
@@ -8,35 +8,48 @@ class ShellCommand extends FaunaCommand {
 		const dbscope = this.args.dbname;
 		const role = 'admin';
 		const log = this.log;
+		const withClient = this.withClient.bind(this)
 
-		this.withClient(function(client) {
-			log(`starting shell for database ${dbscope}`);
+		this.withClient(function(testDbClient) {
+			testDbClient.query(q.Exists(q.Database(dbscope)))
+			.then(function(exists) {
+				if (exists) {
+					withClient(function(client) {
+						log(`starting shell for database ${dbscope}`);
 
-			const r	= repl.start({
-				prompt: 'faunadb> ',
-				ignoreUndefined: true
-			});
+						const r	= repl.start({
+							prompt: 'faunadb> ',
+							ignoreUndefined: true
+						});
 
-			const query = function (exp) {
-				client.query(exp)
-				.then(function(res) {
-					console.log("\n", res);
-					r.displayPrompt();
-				})
-				.catch(function(error) {
-					console.log("\n", "Error:", error.message);
-					r.displayPrompt();
-				});
-			};
+						const query = function (exp) {
+							client.query(exp)
+							.then(function(res) {
+								console.log("\n", res);
+								r.displayPrompt();
+							})
+							.catch(function(error) {
+								console.log("\n", "Error:", error.message);
+								r.displayPrompt();
+							});
+						};
 
-			Object.assign(r.context, q);
+						Object.assign(r.context, q);
 
-			Object.defineProperty(r.context, 'query', {
-				configurable: false,
-				enumerable: true,
-				value: query
-			});
-		}, dbscope, role);
+						Object.defineProperty(r.context, 'query', {
+							configurable: false,
+							enumerable: true,
+							value: query
+						});
+					}, dbscope, role);
+				} else {
+					console.log(`Database '${dbscope}' doesn't exist`)
+				}
+			})
+			.catch(function(err) {
+				log(err);
+			})
+		})
   }
 }
 
