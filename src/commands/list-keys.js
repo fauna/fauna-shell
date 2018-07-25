2e29f618dc2da1afc155918fe4a77bfd1b484c2f
Alvaro Videla <alvaro.videla@fauna.com>
2018-07-25 22:22:09 +0200

    list-keys command code cleanup

--- src/commands/list-keys.js
+++ src/commands/list-keys.js
@@ -4,6 +4,9 @@ const faunadb = require('faunadb');
 const q = faunadb.query;
 const Table = require('cli-table');
 
+/**
+ * See the cli-table docs: https://github.com/Automattic/cli-table
+ */
 function getTable() {
 	return new Table({
 	  chars: { 'top': '' , 'top-mid': '' , 'top-left': '' , 'top-right': ''
@@ -16,6 +19,12 @@ function getTable() {
 	});
 }
 
+/**
+ * Sorts keys by database name.
+ *
+ * @param {Key} a - Key reference.
+ * @param {Key} b - Key reference.
+ */
 function compareByDBName(a, b) {
 	if (a.database.id < b.database.id) {
 		return -1;
@@ -25,20 +34,32 @@ function compareByDBName(a, b) {
 	return 0;
 }
 
+function buildTable(res) {
+	const table = getTable()
+	res.data.sort(compareByDBName)
+	res.data.forEach(function(el) {
+		table.push([el.ref.id, el.database.id, el.role])
+	})
+	return table;
+}
+
+/**
+ * Despite its name, returns the first 1000 keys defined on the Database.
+ * "1000 keys ought to be enough for anybody".
+ */
+function allKeysQuery(q) {
+	return q.Map(q.Paginate(q.Keys(null), { size: 1000 }), q.Lambda("x", q.Get(q.Var("x"))));
+}
+
 class ListKeysCommand extends FaunaCommand {
 	async run() {
 		const log = this.log;
 		this.withClient(function(client, endpoint) {
 		  log('listing keys');
-		  client.query(q.Map(q.Paginate(q.Keys(null), { size: 1000 }), q.Lambda("x", q.Get(q.Var("x")))))
+		  client.query(allKeysQuery(q))
 		  .then(function(res) {
 				if (res.data.length > 0) {
-					const table = getTable()
-					res.data.sort(compareByDBName)
-					res.data.forEach(function(el) {
-						table.push([el.ref.id, el.database.id, el.role])
-					})
-					log(table.toString()) 
+					log(buildTable(res).toString())
 				} else {
 					log('No keys created')
 				}
