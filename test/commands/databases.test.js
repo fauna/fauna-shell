e2e4de0f1c48d769f2904fac4df031306d1e2a39
Alvaro Videla <alvaro.videla@fauna.com>
2018-08-27 19:28:19 +0200

    moves delete database test to generic databases test file

--- test/commands/databases.test.js
+++ test/commands/databases.test.js
@@ -10,6 +10,15 @@ describe('create-database', () => {
 
   test
   .stdout()
+  .command(['delete-database', 'testdb'])
+  .catch(err => {
+    expect(err.message).to.contain("Database 'testdb' not found")
+    expect(err.oclif.exit).to.equal(1)
+  })
+  .it('runs delete-database testdb')
+
+  test
+  .stdout()
   .command(['create-database', 'testdb'])
   .it('runs create-database testdb', ctx => {
     expect(ctx.stdout).to.contain("created database 'testdb'")
