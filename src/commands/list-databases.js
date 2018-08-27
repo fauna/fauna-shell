a5afc1c590585ab10c3d2ba7bdc3dab826c6699c
Alvaro Videla <alvaro.videla@fauna.com>
2018-08-27 19:24:39 +0200

    adds list-databases tests

--- src/commands/list-databases.js
+++ src/commands/list-databases.js
@@ -1,14 +1,35 @@
 const FaunaCommand = require('../lib/fauna-command.js')
+const {errorOut} = require('../lib/misc.js')
 const faunadb = require('faunadb')
 const q = faunadb.query
 
+/**
+* Despite its name, returns the first 1000 databases defined.
+* "1000 databases ought to be enough for anybody".
+*/
+function allDatabasesQuery(q) {
+  return q.Map(q.Paginate(q.Databases(null), {size: 1000}), q.Lambda('x', q.Get(q.Var('x'))))
+}
+
 class ListDatabasesCommand extends FaunaCommand {
   async run() {
-    this.paginate(
-      q.Databases(null),
-      'listing databases',
-      'No databases created'
-    )
+    const log = this.log
+    return this.withClient(function (client, _) {
+      log('listing databases')
+      return client.query(allDatabasesQuery(q))
+      .then(function (res) {
+        if (res.data.length > 0) {
+          res.data.forEach(function (el) {
+            log(el.ref.id)
+          })
+        } else {
+          log('No databases created')
+        }
+      })
+      .catch(function (err) {
+        errorOut(err.message)
+      })
+    })
   }
 }
 
--- test/commands/create-database.test.js
+++ test/commands/create-database.test.js
@@ -3,12 +3,27 @@ const {expect, test} = require('@oclif/test')
 describe('create-database', () => {
   test
   .stdout()
+  .command(['list-databases'])
+  .it('runs list-databases', ctx => {
+    expect(ctx.stdout).to.contain('No databases created')
+  })
+
+  test
+  .stdout()
   .command(['create-database', 'testdb'])
   .it('runs create-database testdb', ctx => {
     expect(ctx.stdout).to.contain("created database 'testdb'")
   })
 
   test
+  .stdout()
+  .command(['list-databases'])
+  .it('runs list-databases', ctx => {
+    expect(ctx.stdout).to.contain('testdb')
+  })
+
+  test
+  .stdout()
   .command(['create-database', 'testdb'])
   .catch(err => {
     expect(err.message).to.contain("Database 'testdb' already exists.")
@@ -21,5 +36,12 @@ describe('create-database', () => {
   .command(['delete-database', 'testdb'])
   .it('runs delete-database testdb', ctx => {
     expect(ctx.stdout).to.contain("database 'testdb' deleted")
+  })
+
+  test
+  .stdout()
+  .command(['list-databases'])
+  .it('runs list-databases', ctx => {
+    expect(ctx.stdout).to.contain('No databases created')
   })
 })
