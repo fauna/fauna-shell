1d835aa4dc5d14df89a81bbc017559ce4d878985
Alvaro Videla <alvaro.videla@fauna.com>
2018-09-07 20:58:59 +0200

    allows user to connet the shell to their root database
    makes the database name argument for the shell optional
    displays a warning when creating a root shell

--- src/commands/shell.js
+++ src/commands/shell.js
@@ -52,6 +52,83 @@ function filterCommands(commands, unwanted) {
   return filteredCommands
 }
 
+function startShell(client, endpoint, dbscope, log) {
+  const dbname = dbscope ? dbscope : 'root'
+
+  if (dbname === 'root') {
+    log('Warning: You are starting a shell for the root database')
+  } else {
+    log(`Starting shell for database ${dbname}`)
+  }
+
+  log(`Connected to ${stringifyEndpoint(endpoint)}`)
+  log('Type Ctrl+D or .exit to exit the shell')
+  var defaultEval
+
+  function replEvalPromise(cmd, ctx, filename, cb) {
+    if (skipInput(cmd)) {
+      return cb()
+    }
+    defaultEval(cmd, ctx, filename, function (error, result) {
+      if (error) {
+        if (isRecoverableError(error)) {
+          return cb(new repl.Recoverable(error))
+        } else {
+          return cb(error, result)
+        }
+      } else {
+        return client.query(result)
+        .then(function (response) {
+          // we could provide the response result as a second
+          // argument to cb(), but the repl util.inspect has a
+          // default depth of 2, but we want to display the full
+          // objects or arrays, not things like [object Object]
+          console.log(util.inspect(response, {depth: null}))
+          return cb(error)
+        })
+        .catch(function (error) {
+          ctx.lastError = error
+          log('Error:', error.message)
+          return cb()
+        })
+      }
+    })
+  }
+
+  const r	= repl.start({
+    prompt: `${dbname}> `,
+    ignoreUndefined: true,
+  })
+
+  // we don't want to allow people to call some of the default commmands
+  // from the node repl
+  r.commands = filterCommands(r.commands, ['load', 'editor', 'clear'])
+
+  r.defineCommand('clear', {
+    help: 'Clear the repl',
+    action: function () {
+      console.clear()
+      this.displayPrompt()
+    },
+  })
+
+  r.defineCommand('last_error', {
+    help: 'Display the last error',
+    action: function () {
+      console.log(this.context.lastError)
+      this.displayPrompt()
+    },
+  })
+
+  // we define our own eval, because we want to wrap QueryExpressions
+  // inside a FaunaDB's Query().
+  defaultEval = r.eval
+  r.eval = replEvalPromise
+
+  r.context.lastError = undefined
+  Object.assign(r.context, q)
+}
+
 class ShellCommand extends FaunaCommand {
   async run() {
     const dbscope = this.args.dbname
@@ -59,89 +136,29 @@ class ShellCommand extends FaunaCommand {
     const log = this.log
     const withClient = this.withClient.bind(this)
 
-    // first we test if the database specified by the user exists.
-    // if that's the case, we create a connection scoped to that database.
-    this.withClient(function (testDbClient, _) {
-      testDbClient.query(q.Exists(q.Database(dbscope)))
-      .then(function (exists) {
-        if (exists) {
-          withClient(function (client, endpoint) {
-            log(`Starting shell for database ${dbscope}`)
-            log(`Connected to ${stringifyEndpoint(endpoint)}`)
-            log('Type Ctrl+D or .exit to exit the shell')
-            var defaultEval
-
-            function replEvalPromise(cmd, ctx, filename, cb) {
-              if (skipInput(cmd)) {
-                return cb()
-              }
-              defaultEval(cmd, ctx, filename, function (error, result) {
-                if (error) {
-                  if (isRecoverableError(error)) {
-                    return cb(new repl.Recoverable(error))
-                  } else {
-                    return cb(error, result)
-                  }
-                } else {
-                  return client.query(result)
-                  .then(function (response) {
-                    // we could provide the response result as a second
-                    // argument to cb(), but the repl util.inspect has a
-                    // default depth of 2, but we want to display the full
-                    // objects or arrays, not things like [object Object]
-                    console.log(util.inspect(response, {depth: null}))
-                    return cb(error)
-                  })
-                  .catch(function (error) {
-                    ctx.lastError = error
-                    log('Error:', error.message)
-                    return cb()
-                  })
-                }
-              })
-            }
-
-            const r	= repl.start({
-              prompt: `${dbscope}> `,
-              ignoreUndefined: true,
-            })
-
-            // we don't want to allow people to call some of the default commmands
-            // from the node repl
-            r.commands = filterCommands(r.commands, ['load', 'editor', 'clear'])
-
-            r.defineCommand('clear', {
-              help: 'Clear the repl',
-              action: function () {
-                console.clear()
-                this.displayPrompt()
-              },
-            })
-
-            r.defineCommand('last_error', {
-              help: 'Display the last error',
-              action: function () {
-                console.log(this.context.lastError)
-                this.displayPrompt()
-              },
-            })
-
-            // we define our own eval, because we want to wrap QueryExpressions
-            // inside a FaunaDB's Query().
-            defaultEval = r.eval
-            r.eval = replEvalPromise
-
-            r.context.lastError = undefined
-            Object.assign(r.context, q)
-          }, dbscope, role)
-        } else {
-          errorOut(`Database '${dbscope}' doesn't exist`, 1)
-        }
+    if (dbscope) {
+      // first we test if the database specified by the user exists.
+      // if that's the case, we create a connection scoped to that database.
+      this.withClient(function (testDbClient, _) {
+        testDbClient.query(q.Exists(q.Database(dbscope)))
+        .then(function (exists) {
+          if (exists) {
+            withClient(function (client, endpoint) {
+              startShell(client, endpoint, dbscope, log)
+            }, dbscope, role)
+          } else {
+            errorOut(`Database '${dbscope}' doesn't exist`, 1)
+          }
+        })
+        .catch(function (err) {
+          errorOut(err.message, 1)
+        })
       })
-      .catch(function (err) {
-        errorOut(err.message, 1)
+    } else {
+      withClient(function (client, endpoint) {
+        startShell(client, endpoint, dbscope, log)
       })
-    })
+    }
   }
 }
 
@@ -160,7 +177,7 @@ ShellCommand.flags = {
 ShellCommand.args = [
   {
     name: 'dbname',
-    required: true,
+    required: false,
     description: 'database name',
   },
 ]
