92a8633ed7eeb13e4fe3e4c7f36975d400c4a636
SZinkevych <s.zinkevych@gmail.com>
2021-11-05 16:56:11 +0100

    move ensureRow and ensurHeadr to a parser level

--- src/lib/csv-stream/index.js
+++ src/lib/csv-stream/index.js
@@ -13,6 +13,8 @@ class CSVStream extends Stream {
     this._destroyed = false
     this._endCallWhenPause = false
 
+    this.flags = options.flags
+
     // Buffer
     this._buffer = Buffer.alloc(0)
     this._encoding = undefined // Encoding needs to be undefined for Buffer.toString method
@@ -22,13 +24,14 @@ class CSVStream extends Stream {
     this._parser.on('data', (data) => {
       if (this._ended)
         throw new Error('Must not emit data event after emittion of end event.')
-      this.emit('data', data)
+
+      this.emit('data', this.ensureRows(data))
     })
     this._parser.on('column', (key, value) => {
       this.emit('column', key, value)
     })
     this._parser.on('header', (header) => {
-      this.emit('header', header)
+      this.emit('header', this.ensureHeader(header))
     })
     this._parser.on('end', () => {
       this._ended = true
@@ -79,6 +82,78 @@ class CSVStream extends Stream {
       this.write()
     if (this._endCallWhenPause) this.end()
     this.emit('drain')
+  }
+
+  ensureHeader(headers) {
+    const { duplicates } = headers.reduce(
+      (memo, next) => {
+        const header = next.trim()
+        if (memo.counts[header]) {
+          memo.counts[header]++
+          memo.duplicates.add(header)
+        }
+
+        memo.counts[header] = 1
+        return memo
+      },
+      {
+        counts: {},
+        duplicates: new Set(),
+      }
+    )
+
+    if (duplicates.size) {
+      this.emit(
+        'error',
+        new Error(
+          `File should not have duplicates headers. Please check following header(s): ${[
+            ...duplicates,
+          ]}`
+        )
+      )
+      return
+    }
+
+    const invalid = headers.filter(
+      (fieldName) =>
+        !/^[a-zA-Z0-9=:;_|!@#$%&~^(){}\t \-\+\.\?]+$/.test(fieldName.trim())
+    )
+
+    if (invalid.length > 0) {
+      this.emit(
+        'error',
+        new Error(
+          `${invalid} field(s) has invalid characters. Supported: [a-zA-Z0-9=:;_|!@#$%&~^(){}\\t \-\+\.\?]+`
+        )
+      )
+    }
+
+    return headers
+  }
+
+  ensureRows(row) {
+    const headerCount = this._parser.columns.length
+    const rowColumnCount = Object.keys(row).length
+    const rowNumber = this._parser._index + 1
+    if (rowColumnCount > headerCount) {
+      this.emit(
+        'error',
+        new Error(
+          `Row ${rowNumber} has more columns that described at the headers`
+        )
+      )
+      return
+    }
+    if (rowColumnCount < headerCount && !this.flags['allow-short-rows']) {
+      this.emit(
+        'error',
+        new Error(
+          `Row ${rowNumber} has less columns than headers. Add '--allow-short-rows' to allow rows which are shorter than the number of headers`
+        )
+      )
+    }
+
+    return row
   }
 }
 
