c31bc2fe5c3918851af096c7c9e7b414941b1f90
Neil Macneale IV <neil.macneale@fauna.com>
2021-11-08 15:55:33 -0800

    csv-parser refactored

--- src/lib/csv-stream/index.js
+++ src/lib/csv-stream/index.js
@@ -8,90 +8,89 @@
  * https://github.com/lbdremy/node-csv-stream/blob/8aea01108b/index.js
  */
 
-/**
- * Modules dependencies
- */
+const Stream = require('stream')
+const Parser = require('./parser')
 
-var Stream = require('stream'),
-	util = require('util'),
-	Parser = require('./lib/parser');
+class CSVStream extends Stream {
+  constructor(options) {
+    super()
 
-exports.createStream = function(options){
-	return new CSVStream(options || {});
+    // States
+    this.writable = true
+    this.readable = true
+    this._paused = false
+    this._ended = false
+    this._destroyed = false
+    this._endCallWhenPause = false
+
+    // Buffer
+    this._buffer = Buffer.alloc(0)
+    this._encoding = undefined // Encoding needs to be undefined for Buffer.toString method
+
+    // CSV parser
+    this._parser = new Parser(options)
+    this._parser.on('data', (data) => {
+      if (this._ended)
+        throw new Error('Must not emit data event after emittion of end event.')
+      this.emit('data', data)
+    })
+    this._parser.on('column', (key, value) => {
+      this.emit('column', key, value)
+    })
+    this._parser.on('header', (header) => {
+      this.emit('header', header)
+    })
+    this._parser.on('end', () => {
+      this._ended = true
+      this.readable = false
+      this.emit('end')
+    })
+  }
+
+  write(buffer, encoding) {
+    this._encoding = encoding || this._encoding
+    if (this._ended) throw new Error('Cannot write after end has been called.')
+    if (buffer)
+      this._buffer = Buffer.concat(
+        [this._buffer, buffer],
+        this._buffer.length + buffer.length
+      )
+    if (this._paused) return false
+    this._parser.parse(this._buffer.toString(this._encoding))
+    this._buffer = Buffer.alloc(0)
+    return !this._paused
+  }
+
+  end(buffer, encoding) {
+    if (this._buffer || buffer) {
+      if (this.write(buffer, encoding)) {
+        this.writable = false
+        this._parser.end()
+        if (!this._destroyed) this.destroy()
+      } else {
+        this._endCallWhenPause = true
+      }
+    }
+  }
+
+  destroy() {
+    this._buffer = null
+    this._destroyed = true
+    this.emit('close')
+  }
+
+  pause() {
+    this._paused = true
+  }
+
+  resume() {
+    this._paused = false
+    if (this._buffer && this._buffer.length > 0 && !this._endCallWhenPause)
+      this.write()
+    if (this._endCallWhenPause) this.end()
+    this.emit('drain')
+  }
 }
 
-function CSVStream(options){
-	var self = this;
-	Stream.call(this);
+module.exports = CSVStream
 
-	// States
-	this.writable = true;
-	this.readable = true;
-	this._paused = false;
-	this._ended = false;
-	this._destroyed = false;
-	this._endCallWhenPause = false;
-
-	// Buffer
-	this._buffer = new Buffer(0);
-	this._encoding = undefined; // Encoding needs to be undefined for Buffer.toString method
-
-	// CSV parser
-	this._parser = new Parser(options);
-	this._parser.on('data',function(data){
-		if(self._ended) throw new Error('Must not emit data event after emittion of end event.')
-		self.emit('data',data);
-	});
-	this._parser.on('column',function(key,value){
-		self.emit('column',key,value);
-	});
-	this._parser.on('header',function(header){
-		self.emit('header',header);
-	});
-	this._parser.on('end',function(){
-		self._ended = true;
-		self.readable = false;
-		self.emit('end');
-	});
-}
-
-util.inherits(CSVStream,Stream);
-
-CSVStream.prototype.write = function(buffer,encoding){
-	this._encoding = encoding || this._encoding;
-	if(this._ended) throw new Error('Cannot write after end has been called.');
-	if(buffer) this._buffer = Buffer.concat([this._buffer, buffer], this._buffer.length + buffer.length);
-	if(this._paused) return false;
-	this._parser.parse(this._buffer.toString(this._encoding));
-	this._buffer = new Buffer(0);
-	return !this._paused;
-}
-
-CSVStream.prototype.end = function(buffer,encoding){
-	if(this._buffer || buffer){
-		if(this.write(buffer,encoding)){
-			this.writable = false;
-			this._parser.end();
-			if(!this._destroyed) this.destroy();
-		}else{
-			this._endCallWhenPause = true;
-		}
-	}
-}
-
-CSVStream.prototype.destroy = function(){
-	this._buffer = null;
-	this._destroyed = true;
-	this.emit('close');
-}
-
-CSVStream.prototype.pause = function(){
-	this._paused = true;
-}
-
-CSVStream.prototype.resume = function(){
-	this._paused = false;
-	if(this._buffer && this._buffer.length > 0 && !this._endCallWhenPause) this.write();
-	if(this._endCallWhenPause) this.end();
-	this.emit('drain');
-}
