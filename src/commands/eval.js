2c65992505cf4133fb4d210382c48f5d0f379c52
Victor Duarte <zvictor@users.noreply.github.com>
2022-03-08 23:53:39 +0100

    Forward error to stderr and set exitCode

--- src/commands/eval.js
+++ src/commands/eval.js
@@ -59,12 +59,14 @@ function performQuery(client, fqlQuery, outputFile, outputFormat) {
       return writeFormattedOutput(outputFile, response, outputFormat)
     })
     .catch(function (error) {
-      console.log(
+      console.error(
         util.inspect(JSON.parse(error.faunaError.requestResult.responseRaw), {
           depth: null,
           compact: false,
         })
       )
+    
+      process.exit(1)
     })
 }
 
