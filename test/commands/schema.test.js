b2f5527f5799ced0df4eeb1ffed77c0238d63619
Will Berkeley <will.berkeley@fauna.com>
2023-07-20 14:45:14 -0700

    ENG-5274: Implement fauna schema:pull

--- test/commands/schema.test.js
+++ test/commands/schema.test.js
@@ -1,6 +1,7 @@
 const { expect, test } = require("@oclif/test");
 const { query: q } = require("faunadb");
 const { withOpts, getEndpoint, matchFqlReq } = require("../helpers/utils.js");
+const { ux } = require("@oclif/core");
 
 const files = {
   version: 0,
@@ -14,6 +15,11 @@ const files = {
 const main = {
   version: 0,
   content: "collection foo {}\n\nfunction bar(x, y) { x + y }\n",
+};
+
+const diff = {
+  version: 0,
+  diff: "main.fsl ADD collection foo",
 };
 
 describe("fauna schema:ls test", () => {
@@ -49,5 +55,25 @@ describe("fauna schema:pull test", () => {
     .command(withOpts(["schema:pull", "main.fsl"]))
     .it("runs schema:pull", (ctx) => {
       expect(ctx.stdout).to.contain(`${main.content}`);
+    });
+});
+
+describe("fauna schema:push test", () => {
+  test
+    .stub(ux, "confirm", () => async () => true)
+    .nock(getEndpoint(), { allowUnmocked: false }, (api) =>
+      api
+        .persist()
+        .post("/", matchFqlReq(q.Now()))
+        .reply(200, new Date())
+        .post("/schema/1/validate")
+        .reply(200, diff)
+        .post("/schema/1/update?version=0")
+        .reply(200)
+    )
+    .stdout()
+    .command(withOpts(["schema:push", "main.fsl"]))
+    .it("runs schema:push", (ctx) => {
+      expect(ctx.stdout).to.contain(`${diff.diff}`);
     });
 });
