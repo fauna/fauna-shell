214cb12f9d26eacfb62a31d0f8c0f80b13f53891
Neil Macneale V <neil.macneale.v@gmail.com>
2023-08-28 16:25:11 -0700

    Fix tests

--- src/commands/shell.js
+++ src/commands/shell.js
@@ -52,9 +52,13 @@ class ShellCommand extends EvalCommand {
     });
     // The repl normally waits for the HTTP2 stream to close, so it just hangs
     // on v10. This causes it to actually die.
-    this.repl.on('exit', (code) => {
-      process.exit(code);
-    });
+    //
+    // Also, tests don't like `process.exit()`, so don't do this in tests.
+    if (typeof global.it !== 'function') {
+      this.repl.on('exit', (code) => {
+        process.exit(code);
+      });
+    }
     this.repl.eval = this.eval(this.repl.eval);
     this.repl.context.lastError = undefined;
     Object.assign(this.repl.context, q);
