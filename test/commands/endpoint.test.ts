5d1a0b3d5e2501c5c95e3e4027073fc1d12a3817
Neil Macneale V <neil.macneale.v@gmail.com>
2023-10-03 13:07:31 -0700

    Add tests for the remaining endpoint commands

--- test/commands/endpoint.test.ts
+++ test/commands/endpoint.test.ts
@@ -1,7 +1,9 @@
 import { expect, test } from "@oclif/test";
 import { ShellConfig, getRootConfigPath } from "../../src/lib/config";
 import sinon, { SinonStub } from "sinon";
-import AddEndpointCommand from "../../src/commands/add-endpoint";
+import AddEndpointCommand from "../../src/commands/endpoint/add";
+import ListEndpointCommand from "../../src/commands/endpoint/list";
+import RemoveEndpointCommand from "../../src/commands/endpoint/remove";
 import { Config } from "@oclif/core";
 
 const rootConfigPath = getRootConfigPath();
@@ -18,7 +20,7 @@ const stubbedRootConfig = (
   return config as any;
 };
 
-describe("add-endpoint", () => {
+describe("endpoint:add", () => {
   test
     .add("config", () =>
       stubbedRootConfig({
@@ -45,7 +47,7 @@ describe("add-endpoint", () => {
     )
     .it("adds an endpoint", (ctx) => {
       expect(ctx.stdout).to.equal(
-        `Saved endpoint foobar to ${rootConfigPath}\n`
+        `Warning: could not connect to Fauna\nSaved endpoint foobar to ${rootConfigPath}\n`
       );
       expect(ctx.config.rootConfig).to.deep.equal({
         defaultEndpoint: "my-endpoint",
@@ -96,7 +98,7 @@ describe("add-endpoint", () => {
     )
     .it("sets default endpoint", (ctx) => {
       expect(ctx.stdout).to.equal(
-        `Saved endpoint foobar to ${rootConfigPath}\n`
+        `Warning: could not connect to Fauna\nSaved endpoint foobar to ${rootConfigPath}\n`
       );
       expect(ctx.config.rootConfig).to.deep.equal({
         defaultEndpoint: "foobar",
@@ -112,6 +114,112 @@ describe("add-endpoint", () => {
           foobar: {
             url: "http://foo.baz",
             secret: "fn1234",
+            graphqlHost: "graphql.fauna.com",
+            graphqlPort: 443,
+          },
+        },
+      });
+      expect(ctx.config.saveRootConfig.calledOnce).to.be.true;
+    });
+});
+
+describe("endpoint:list", () => {
+  test
+    .add("config", () =>
+      stubbedRootConfig({
+        default: "my-endpoint",
+        "my-endpoint": {
+          url: "http://bar.baz",
+          secret: "fn3333",
+        },
+        "other-endpoint": {
+          url: "http://bar.baz",
+          secret: "fn3333",
+        },
+      })
+    )
+    .stdout()
+    .do((ctx) =>
+      new ListEndpointCommand([], new Config({} as any)).execute(ctx.config)
+    )
+    .it("lists endpoints", (ctx) => {
+      expect(ctx.stdout).to.equal(
+        `Available endpoints:\n* my-endpoint\n  other-endpoint\n`
+      );
+      expect(ctx.config.saveRootConfig.calledOnce).to.be.false;
+    });
+});
+
+describe("endpoint:remove", () => {
+  test
+    .add("config", () =>
+      stubbedRootConfig({
+        default: "my-endpoint",
+        "my-endpoint": {
+          url: "http://bar.baz",
+          secret: "fn3333",
+        },
+        "other-endpoint": {
+          url: "http://bar.baz",
+          secret: "fn3333",
+        },
+      })
+    )
+    .stdout()
+    .do((ctx) =>
+      new RemoveEndpointCommand(
+        ["other-endpoint"],
+        new Config({} as any)
+      ).execute(ctx.config)
+    )
+    .it("removes an endpoint", (ctx) => {
+      expect(ctx.stdout).to.equal(`Removed endpoint other-endpoint.\n`);
+      expect(ctx.config.rootConfig).to.deep.equal({
+        defaultEndpoint: "my-endpoint",
+        endpoints: {
+          "my-endpoint": {
+            url: "http://bar.baz",
+            secret: "fn3333",
+            // These graphql bits are only saved if they differ from the
+            // default.
+            graphqlHost: "graphql.fauna.com",
+            graphqlPort: 443,
+          },
+        },
+      });
+      expect(ctx.config.saveRootConfig.calledOnce).to.be.true;
+    });
+
+  test
+    .add("config", () =>
+      stubbedRootConfig({
+        default: "my-endpoint",
+        "my-endpoint": {
+          url: "http://bar.baz",
+          secret: "fn3333",
+        },
+        "other-endpoint": {
+          url: "http://bar.baz",
+          secret: "fn3333",
+        },
+      })
+    )
+    .stdout()
+    .do((ctx) =>
+      new RemoveEndpointCommand(["my-endpoint"], new Config({} as any)).execute(
+        ctx.config
+      )
+    )
+    .it("clears the default if needed", (ctx) => {
+      expect(ctx.stdout).to.equal(`Removed endpoint my-endpoint.\n`);
+      expect(ctx.config.rootConfig).to.deep.equal({
+        defaultEndpoint: undefined,
+        endpoints: {
+          "other-endpoint": {
+            url: "http://bar.baz",
+            secret: "fn3333",
+            // These graphql bits are only saved if they differ from the
+            // default.
             graphqlHost: "graphql.fauna.com",
             graphqlPort: 443,
           },
