0ed66f48d6e83a1276c402e6833176ca641d5f6a
Matt Wilde <mwilde345@gmail.com>
2024-07-16 11:32:05 -0400

    wip test fix and nock upgrade migration

--- test/commands/databases.test.js
+++ test/commands/databases.test.js
@@ -1,4 +1,6 @@
-const { expect, test } = require("@oclif/test");
+import { expect } from "chai";
+import { runCommand } from "@oclif/test";
+import nock from "nock";
 const { query: q, values } = require("faunadb");
 const { withOpts, getEndpoint, matchFqlReq } = require("../helpers/utils.js");
 
@@ -14,103 +16,92 @@ const databases = [
 ];
 
 describe("database test", () => {
-  test
-    .timeout(7000)
-    .nock(getEndpoint(), { allowUnmocked: true }, (api) =>
-      api
-        .persist()
-        .post("/", matchFqlReq(q.Now()))
-        .reply(200, new Date())
-        .post("/", matchFqlReq(q.Paginate(q.Databases(null), { size: 1000 })))
-        .reply(200, { resource: { data: databases } })
-    )
-    .stdout()
-    .command(withOpts(["list-databases"]))
-    .it("runs list-databases", (ctx) => {
-      expect(ctx.stdout).to.contain("listing databases\ntest\ntest2");
-    });
+  // Translate this test to work with version 4.0.4 of @oclif/test
 
-  test
-    .nock(getEndpoint(), { allowUnmocked: true }, (api) =>
-      api
-        .persist()
-        .post("/", matchFqlReq(q.Now()))
-        .reply(200, new Date())
-        .post("/", matchFqlReq(q.CreateDatabase({ name: databases[0].name })))
-        .reply(201, {})
-    )
-    .stdout()
-    .command(withOpts(["create-database", databases[0].name]))
-    .it("runs create-databases", (ctx) => {
-      expect(ctx.stdout).to.contain(`created database ${databases[0].name}`);
-    });
+  it("runs list-databases", async () => {
+    nock(getEndpoint(), { allowUnmocked: true })
+      .persist()
+      .post("/", matchFqlReq(q.Now()))
+      .reply(200, new Date())
+      .post("/", matchFqlReq(q.Paginate(q.Databases(), { size: 1000 })))
+      .reply(200, { resource: { data: databases } });
+    const { stdout } = await runCommand(withOpts(["list-databases"]));
+    expect(stdout).to.contain("listing databases\ntest\ntest2");
+  }, 7000);
 
-  test
-    .nock(getEndpoint(), { allowUnmocked: true }, (api) =>
-      api
-        .persist()
-        .post("/", matchFqlReq(q.Now()))
-        .reply(200, new Date())
-        .post("/", matchFqlReq(q.CreateDatabase({ name: databases[0].name })))
-        .reply(400, {
-          errors: [
-            {
-              position: ["create_database"],
-              code: "instance already exists",
-              description: "Database already exists.",
-            },
-          ],
-        })
-    )
-    .stdout()
-    .command(withOpts(["create-database", databases[0].name]))
-    .catch((err) => {
+  it("runs create-databases", async () => {
+    nock(getEndpoint(), { allowUnmocked: true })
+      .persist()
+      .post("/", matchFqlReq(q.Now()))
+      .reply(200, new Date())
+      .post("/", matchFqlReq(q.CreateDatabase({ name: databases[0].name })))
+      .reply(201, {});
+    const { stdout } = await runCommand(
+      withOpts(["create-database", databases[0].name])
+    );
+    expect(stdout).to.contain(`created database ${databases[0].name}`);
+  });
+
+  it("runs create-database failed because of existing instance", async () => {
+    nock(getEndpoint(), { allowUnmocked: true })
+      .persist()
+      .post("/", matchFqlReq(q.Now()))
+      .reply(200, new Date())
+      .post("/", matchFqlReq(q.CreateDatabase({ name: databases[0].name })))
+      .reply(400, {
+        errors: [
+          {
+            position: ["create_database"],
+            code: "instance already exists",
+            description: "Database already exists.",
+          },
+        ],
+      });
+    try {
+      await runCommand(withOpts(["create-database", databases[0].name]));
+    } catch (err) {
       expect(err.message).to.contain(
         `Database '${databases[0].name}' already exists`
       );
       expect(err.oclif.exit).to.equal(1);
-    })
-    .it("runs create-database failed because of existing instance");
+    }
+  });
 
-  test
-    .nock(getEndpoint(), { allowUnmocked: true }, (api) =>
-      api
-        .persist()
-        .post("/", matchFqlReq(q.Now()))
-        .reply(200, new Date())
-        .post("/", matchFqlReq(q.Delete(q.Database(databases[0].name))))
-        .reply(200, {})
-    )
-    .stdout()
-    .command(withOpts(["delete-database", databases[0].name]))
-    .it("runs delete-database", (ctx) => {
-      expect(ctx.stdout).to.contain(`database '${databases[0].name}' deleted`);
-    });
+  it("runs delete-database", async () => {
+    nock(getEndpoint(), { allowUnmocked: true })
+      .persist()
+      .post("/", matchFqlReq(q.Now()))
+      .reply(200, new Date())
+      .post("/", matchFqlReq(q.Delete(q.Database(databases[0].name))))
+      .reply(200, {});
+    const { stdout } = await runCommand(
+      withOpts(["delete-database", databases[0].name])
+    );
+    expect(stdout).to.contain(`database '${databases[0].name}' deleted`);
+  });
 
-  test
-    .nock(getEndpoint(), { allowUnmocked: true }, (api) =>
-      api
-        .persist()
-        .post("/", matchFqlReq(q.Now()))
-        .reply(200, new Date())
-        .post("/", matchFqlReq(q.Delete(q.Database(databases[1].name))))
-        .reply(400, () => ({
-          errors: [
-            {
-              position: ["delete"],
-              code: "invalid ref",
-              description: "Ref refers to undefined database 'testing'",
-            },
-          ],
-        }))
-    )
-    .stdout()
-    .command(withOpts(["delete-database", databases[1].name]))
-    .catch((err) => {
+  it("runs delete-database failed because instance not found", async () => {
+    nock(getEndpoint(), { allowUnmocked: true })
+      .persist()
+      .post("/", matchFqlReq(q.Now()))
+      .reply(200, new Date())
+      .post("/", matchFqlReq(q.Delete(q.Database(databases[1].name))))
+      .reply(400, () => ({
+        errors: [
+          {
+            position: ["delete"],
+            code: "invalid ref",
+            description: "Ref refers to undefined database 'testing'",
+          },
+        ],
+      }));
+    try {
+      await runCommand(withOpts(["delete-database", databases[1].name]));
+    } catch (err) {
       expect(err.message).to.contain(
         `Database '${databases[1].name}' not found`
       );
       expect(err.oclif.exit).to.equal(1);
-    })
-    .it("runs delete-database failed because instance not found");
+    }
+  });
 });
