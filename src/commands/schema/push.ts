56677398c187c062983cc7d0d2bf602ac002898b
Neil Macneale V <neil.macneale.v@gmail.com>
2024-08-26 17:23:38 -0700

    Add schema push --stage

--- src/commands/schema/push.ts
+++ src/commands/schema/push.ts
@@ -9,6 +9,11 @@ export default class PushSchemaCommand extends SchemaCommand {
       description: "Push the change without a diff or schema version check",
       default: false,
     }),
+    stage: Flags.boolean({
+      description:
+        "Stages the schema change, instead of applying it immediately",
+      default: false,
+    }),
   };
 
   static description = "Push the current project's .fsl files to Fauna.";
@@ -16,6 +21,7 @@ export default class PushSchemaCommand extends SchemaCommand {
   static examples = [
     "$ fauna schema push",
     "$ fauna schema push --dir schemas/myschema",
+    "$ fauna schema push --stage",
   ];
 
   async run() {
@@ -24,8 +30,14 @@ export default class PushSchemaCommand extends SchemaCommand {
     try {
       const { url, secret } = await this.fetchsetup();
       if (this.flags?.force) {
-        // Just push.
-        const res = await fetch(new URL("/schema/1/update?force=true", url), {
+        const params = new URLSearchParams({
+          force: "true", // Just push.
+          staged: this.flags?.stage ? "true" : "false",
+        });
+
+        // This is how MDN says to do it for some reason.
+        const path = new URL(`/schema/1/update?${params}`, url);
+        const res = await fetch(path, {
           method: "POST",
           headers: { AUTHORIZATION: `Bearer ${secret}` },
           body: this.body(files),
@@ -34,21 +46,30 @@ export default class PushSchemaCommand extends SchemaCommand {
           // @ts-expect-error-next-line
           duplex: "half",
         });
+
         const json = await res.json();
         if (json.error) {
-          this.error(json.error.message);
+          this.error(json.error?.message ?? json.error);
         }
       } else {
-        // Confirm diff, then push it.
-        const res = await fetch(new URL("/schema/1/validate?force=true", url), {
+        // Confirm diff, then push it. `force` is set on `validate` so we don't
+        // need to pass the last known schema version through.
+        const params = new URLSearchParams({
+          force: "true",
+        });
+        const path = new URL(`/schema/1/validate?${params}`, url);
+        const res = await fetch(path, {
           method: "POST",
           headers: { AUTHORIZATION: `Bearer ${secret}` },
           body: this.body(files),
+          // @ts-expect-error-next-line
+          duplex: "half",
         });
         const json = await res.json();
         if (json.error) {
-          this.error(json.error.message);
+          this.error(json.error?.message ?? json.error);
         }
+
         let message = "Accept and push changes?";
         if (json.diff) {
           this.log(`Proposed diff:\n`);
@@ -61,17 +82,22 @@ export default class PushSchemaCommand extends SchemaCommand {
           message,
           default: false,
         });
+
         if (confirmed) {
-          const res = await fetch(
-            new URL(`/schema/1/update?version=${json.version}`, url),
-            {
-              method: "POST",
-              headers: { AUTHORIZATION: `Bearer ${secret}` },
-              body: this.body(files),
-              // @ts-expect-error-next-line
-              duplex: "half",
-            }
-          );
+          const params = new URLSearchParams({
+            version: json.version,
+            staged: this.flags?.stage ? "true" : "false",
+          });
+
+          const path = new URL(`/schema/1/update?${params}`, url);
+          const res = await fetch(path, {
+            method: "POST",
+            headers: { AUTHORIZATION: `Bearer ${secret}` },
+            body: this.body(files),
+            // @ts-expect-error-next-line
+            duplex: "half",
+          });
+
           const json0 = await res.json();
           if (json0.error) {
             this.error(json0.error.message);
