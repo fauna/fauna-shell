7eb96181aa100f291958ddcfd203cb8e5b512533
Ashton Eby <ashton.eby@gmail.com>
2024-10-03 14:53:55 -0700

    add first draft of remaining schema tests

---
+++ yargs-test/schema/abandon.mjs
@@ -0,0 +1,18 @@
+import { expect } from 'chai'
+
+import * as awilix from "awilix/lib/awilix.module.mjs";
+
+import { f } from '../helpers.mjs'
+
+import { run } from '../../src/cli.mjs'
+import { setupTestContainer as setupContainer } from '../../src/config/setup-test-container.mjs'
+
+
+describe('schema abandon', function() {
+  beforeEach(() => {
+    container = setupContainer()
+  })
+
+  it.skip('...?', async function() {
+  })
+})
---
+++ yargs-test/schema/status.mjs
@@ -0,0 +1,18 @@
+import { expect } from 'chai'
+
+import * as awilix from "awilix/lib/awilix.module.mjs";
+
+import { f } from '../helpers.mjs'
+
+import { run } from '../../src/cli.mjs'
+import { setupTestContainer as setupContainer } from '../../src/config/setup-test-container.mjs'
+
+
+describe('schema status', function() {
+  beforeEach(() => {
+    container = setupContainer()
+  })
+
+  it.skip('...?', async function() {
+  })
+})
