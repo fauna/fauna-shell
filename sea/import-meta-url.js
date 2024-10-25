b77aab3204467b1fa70a21842fe58368a10a19e1
Ashton Eby <ashton.eby@gmail.com>
2024-10-25 15:20:59 -0700

    build SEA binaries with github action
    this change
    - adds a github action to build SEA binaries for linux-x64, windows-x64,
      mac-x64, and mac-arm64
    - adds documentation for the SEA build

--- sea/import-meta-url.js
+++ sea/import-meta-url.js
@@ -1,1 +1,4 @@
-export let importMetaUrl = require('url').pathToFileURL(__filename);
+// this is being used as an eslint plugin to map from CJS' __filename/__dirname
+// to ESM's meta.import.url. this lets us write statements using meta.import.url
+// in our source code that actually use CJS primitives after build.
+export let importMetaUrl = require("url").pathToFileURL(__filename);
