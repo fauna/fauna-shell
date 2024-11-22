1759832650a059b4016dbe4fe341ffa357ba2f37
Henry Ball <henry.ball@fauna.com>
2024-11-22 10:38:25 -0800

    stub out create database

---
+++ src/commands/database/create.mjs
@@ -0,0 +1,28 @@
+//@ts-check
+
+import { container } from "../../cli.mjs";
+
+async function createDatabase(argv) {
+  const logger = container.resolve("logger");
+  logger.stdout(`TBD dog`);
+}
+
+function buildCreateCommand(yargs) {
+  return yargs
+    .options({
+      name: {
+        type: "string",
+        description: "the name of the database to create",
+      },
+    })
+    .demandOption("name")
+    .version(false)
+    .help("help", "show help");
+}
+
+export default {
+  command: "create",
+  description: "Creates a database",
+  builder: buildCreateCommand,
+  handler: createDatabase,
+};
