1e6a2eaeac544eb909a8386ec8198d36d8397250
henryfauna <90654917+henryfauna@users.noreply.github.com>
2024-12-04 11:26:41 -0800

    [FE-6155] - Support `--database` in database commands (#454)
    * support --database in all database commands
    
    * support --database in database create
    
    * support --database in database delete
    
    * test new validation
    
    * unclobber logging change
    
    * fix test
    
    * don't duplicate refresh logic
    
    * put common validation in one place
    
    * fix tests
    
    * appease linter

--- test/database/create.mjs
+++ test/database/create.mjs
@@ -6,62 +6,41 @@ import sinon from "sinon";
 
 import { run } from "../../src/cli.mjs";
 import { setupTestContainer as setupContainer } from "../../src/config/setup-test-container.mjs";
+import { mockAccessKeysFile } from "../helpers.mjs";
 
 describe("database create", () => {
-  let container, logger, runQuery;
+  let container, logger, runQuery, makeAccountRequest;
 
   beforeEach(() => {
     // reset the container before each test
     container = setupContainer();
     logger = container.resolve("logger");
     runQuery = container.resolve("faunaClientV10").runQuery;
+    makeAccountRequest = container.resolve("makeAccountRequest");
   });
-
-  [{ missing: "name", command: "database create --secret 'secret'" }].forEach(
-    ({ missing, command }) => {
-      it(`requires a ${missing}`, async () => {
-        try {
-          await run(command, container);
-        } catch (e) {}
-
-        expect(logger.stderr).to.have.been.calledWith(sinon.match(`Missing required argument: ${missing}`));
-        expect(container.resolve("parseYargs")).to.have.been.calledOnce;
-      });
-    },
-  );
 
   [
     {
-      args: "--name 'testdb' --secret 'secret'",
-      expected: { name: "testdb", secret: "secret" },
+      command: "database create --secret 'secret'",
+      message: "Missing required argument: name",
     },
     {
-      args: "--name 'testdb' --secret 'secret' --typechecked",
-      expected: { name: "testdb", secret: "secret", typechecked: true },
+      command: "database create --database 'us-std/example'",
+      message: "Missing required argument: name",
     },
     {
-      args: "--name 'testdb' --secret 'secret' --protected",
-      expected: { name: "testdb", secret: "secret", protected: true },
+      command: "database create --name 'testdb'",
+      message:
+        "No secret or database provided. Please use either --secret or --database.",
     },
-    {
-      args: "--name 'testdb' --secret 'secret' --priority 10",
-      expected: { name: "testdb", secret: "secret", priority: 10 },
-    },
-  ].forEach(({ args, expected }) => {
-    describe("calls fauna with the user specified arguments", () => {
-      it(`${args}`, async () => {
-        await run(`database create ${args}`, container);
-        expect(runQuery).to.have.been.calledOnceWith({
-          url: sinon.match.string,
-          secret: expected.secret,
-          query: fql`Database.create({
-            name: ${expected.name},
-            protected: ${expected.protected ?? null},
-            typechecked: ${expected.typechecked ?? null},
-            priority: ${expected.priority ?? null},
-          })`,
-        });
-      });
+  ].forEach(({ command, message }) => {
+    it(`validates invalid arguments: ${command}`, async () => {
+      try {
+        await run(command, container);
+      } catch (e) {}
+
+      expect(logger.stderr).to.have.been.calledWith(sinon.match(message));
+      expect(container.resolve("parseYargs")).to.have.been.calledOnce;
     });
   });
 
@@ -91,7 +70,139 @@ describe("database create", () => {
         );
       } catch (e) {}
 
-      expect(logger.stderr).to.have.been.calledWith(sinon.match(expectedMessage));
+      expect(logger.stderr).to.have.been.calledWith(
+        sinon.match(expectedMessage),
+      );
+    });
+  });
+
+  describe("if --secret is provided", () => {
+    [
+      {
+        args: "--name 'testdb' --secret 'secret'",
+        expected: { name: "testdb", secret: "secret" },
+      },
+      {
+        args: "--name 'testdb' --secret 'secret' --typechecked",
+        expected: { name: "testdb", secret: "secret", typechecked: true },
+      },
+      {
+        args: "--name 'testdb' --secret 'secret' --protected",
+        expected: { name: "testdb", secret: "secret", protected: true },
+      },
+      {
+        args: "--name 'testdb' --secret 'secret' --priority 10",
+        expected: { name: "testdb", secret: "secret", priority: 10 },
+      },
+    ].forEach(({ args, expected }) => {
+      it(`calls fauna with the correct args: ${args}`, async () => {
+        await run(`database create ${args}`, container);
+
+        expect(runQuery).to.have.been.calledOnceWith({
+          secret: expected.secret,
+          url: sinon.match.string,
+          query: fql`
+            Database.create({
+              name: ${expected.name},
+              protected: ${expected.protected ?? null},
+              typechecked: ${expected.typechecked ?? null},
+              priority: ${expected.priority ?? null},
+            })
+          `,
+        });
+
+        // If we are using a user provided secret, we should not
+        // need to call the account api to mint or refresh a key.
+        expect(makeAccountRequest).to.not.have.been.called;
+      });
+    });
+
+    it("does not try to refresh the secret if it is invalid", async () => {
+      runQuery.rejects(
+        new ServiceError(
+          {
+            error: {
+              code: "unauthorized",
+              message: "invalid secret",
+            },
+          },
+          401,
+        ),
+      );
+
+      try {
+        await run(
+          `database create --name 'testdb' --secret 'secret' --verbosity=9001`,
+          container,
+        );
+      } catch (e) {}
+
+      expect(makeAccountRequest).to.not.have.been.called;
+      expect(logger.stderr).to.have.been.calledWith(
+        sinon.match(
+          "Authentication failed: Please either log in using 'fauna login' or provide a valid database secret with '--secret'.",
+        ),
+      );
+    });
+  });
+
+  describe("if --database is provided", () => {
+    [
+      {
+        args: "--name 'testdb' --database 'us-std/example'",
+        expected: { name: "testdb", database: "us-std/example" },
+      },
+      {
+        args: "--name 'testdb' --database 'us-std/example' --typechecked",
+        expected: {
+          name: "testdb",
+          database: "us-std/example",
+          typechecked: true,
+        },
+      },
+      {
+        args: "--name 'testdb' --database 'us-std/example' --protected",
+        expected: {
+          name: "testdb",
+          database: "us-std/example",
+          protected: true,
+        },
+      },
+      {
+        args: "--name 'testdb' --database 'us-std/example' --priority 10",
+        expected: { name: "testdb", database: "us-std/example", priority: 10 },
+      },
+    ].forEach(({ args, expected }) => {
+      it(`calls fauna with the correct args: ${args}`, async () => {
+        mockAccessKeysFile({ fs: container.resolve("fs") });
+        // We will attempt to mint a new database key, mock the response
+        // so we can verify that the new key is used.
+        makeAccountRequest.resolves({ secret: "new-secret" });
+
+        await run(`database create ${args}`, container);
+
+        // Verify that we made a request to mint a new database key.
+        expect(makeAccountRequest).to.have.been.calledOnceWith({
+          method: "POST",
+          path: "/databases/keys",
+          body: sinon.match((value) => value.includes(expected.database)),
+          secret: sinon.match.string,
+        });
+
+        // Verify that we made a request to create the database with the new key.
+        expect(runQuery).to.have.been.calledOnceWith({
+          secret: "new-secret",
+          url: sinon.match.string,
+          query: fql`
+            Database.create({
+              name: '${expected.name}',
+              protected: ${expected.protected ?? null},
+              typechecked: ${expected.typechecked ?? null},
+              priority: ${expected.priority ?? null},
+            })
+          `,
+        });
+      });
     });
   });
 });
